58e5b73504f6b904d1b2eb75e14cbcb8d5c730d9
next-9527 <60825099+next-9527@users.noreply.github.com>
2021-05-27 01:38:22 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -33,7 +33,7 @@ module.exports = {
   
     
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: "next-9527 / nextjs-notion-starter-kit",
+  utterancesGitHubRepo: "next-9527/nextjs-notion-starter-kit",
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
