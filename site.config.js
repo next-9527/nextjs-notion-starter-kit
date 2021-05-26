d59c7986efc02e2c395d9c5a7cdfecb6a18827ab
next-9527 <60825099+next-9527@users.noreply.github.com>
2021-05-27 00:19:07 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -34,10 +34,10 @@ module.exports = {
   // Utteranc.es comments via GitHub issue comments (optional)
   utterancesGitHubRepo: null,
   
-   cusdis: {
-  appId: '9f0910f4-a295-4612-a1b5-45fd5f3ba8f0' // your website id}, 
+  cusdis: {
+  appId: '9f0910f4-a295-4612-a1b5-45fd5f3ba8f0' // your website id
+   }, 
 
-  
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
   // variables specified in .env.example
